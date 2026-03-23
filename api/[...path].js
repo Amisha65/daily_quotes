ce8f93978453baa75d46ab913c92f0503cae47a1
Amisha65 <amishadh00@gmail.com>
2026-03-23 20:26:01 +0530

    Fix Vercel API routing handler

--- api/[...path].js
+++ api/[...path].js
@@ -1,13 +1,16 @@
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
 const connectDB = require("../Server/config/db");
 const app = require("../Server/app");
 
 let connectionPromise;
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (!connectionPromise) {
     connectionPromise = connectDB(process.env.MONGO_URI);
   }
 
   await connectionPromise;
   return app(req, res);
-};
+}
